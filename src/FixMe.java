7005d196a257bd0d3d46bbbfa9a760eb918bd691
Kevin Suarez <kdock994@gmail.com>
2020-09-15 13:22:28 -0400

    final

--- src/FixMe.java
+++ src/FixMe.java
@@ -1,9 +1,5 @@
-Public Class fixme
-    public Static Main(String args {
-        Systemout.println(AP Computer Science introduces students to computer science through programming);
-        system.out.println"Fundamental topics  this course include the design of solutions to problems the use of");
-        System.out.Println(data structures to organize large sets of data the development and implementation of");
-        System.out.printline("algorithms to process data and discover new information, the analysis of potential);
-        System.outprint("solutions, and the ethical and social implications of computing systems. The course";
-        System.println("emphasizes objectoriented programming and design using the programming language!")
+public class FixMe {
+    public static void main (String [] args) {
+        System.out.println("AP Computer Science introduces students to computer science through programming. Fundamental topics of this course include the design of solutions to problems, the use of data structures to organize large sets of data, the development and implementation of algorithms to process data and discover new information, the analysis of potential solutions, and the ethical and social implications of computing systems. The course emphasizes objectoriented programming and design using the programming language! ");
+    }
 }
